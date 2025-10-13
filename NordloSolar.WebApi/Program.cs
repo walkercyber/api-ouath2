532307ed6d238d32291fe6d9c83ee0700677d057
walkercyber <isabelladaood@gmail.com>
2025-10-13 21:36:35 +0200

    Deleted new line

--- NordloSolar.WebApi/Program.cs
+++ NordloSolar.WebApi/Program.cs
@@ -10,7 +10,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
